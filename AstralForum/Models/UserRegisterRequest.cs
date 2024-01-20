a010e7d9ae01d99c3ea977a96dc026980c6fc609
Iavor Vassilev <yav.vasilev@gmail.com>
2024-01-20 15:56:05 +0200

    Basic project with scaffolded identity

---
+++ AstralForum/Models/UserRegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AstralForum.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(4, ErrorMessage = "Please enter at least 4 characters!")]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
